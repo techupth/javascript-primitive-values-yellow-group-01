642f48291bd12c8ecb1c9876f89648041350bc91
Gun <cobaltgun2324@gmail.com>
2023-11-23 15:12:45 +0700

    feat: add complete exercise

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let discountPrice = totalPrice * (25 / 100);
+let netPrice = totalPrice - discountPrice;
+console.log(netPrice);
--- ex-6.js
+++ ex-6.js
@@ -2,5 +2,6 @@
 
 let totalParts = 13000;
 
-let concatSentence;
-let literalSentence;
+let concatSentence = "Total:" + totalParts + " Machine parts";
+let literalSentence = `Total: ${totalParts} Machine parts`;
+console.log(literalSentence);
